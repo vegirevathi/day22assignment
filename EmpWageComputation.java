4d502edc9df75d4a6e5ff740d5954b88aa82cc6d
vegirevathi <vegirevathi@gmail.com>
2020-05-25 19:06:29 +0530

    Computing Emp Wage for multiple companies in UC8

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,13 +1,22 @@
 public class EmpWageComputation {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_WORKING_HRS = 100;
+
+	private final String company;
+	private final int empRate;
+	private final int numOfDays;
+	private final int maxHrs;
    
-	public static int computeEmpWage() {
-		int empHrs, empWage, totalWage = 0, totalEmpHrs=0, totalWorkingDays=0 ;
-		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+	public EmpWageComputation(String company, int empRate, int numOfDays, int maxHrs){
+		this.company = company;
+		this.empRate = empRate;
+		this.numOfDays = numOfDays;
+		this.maxHrs = maxHrs;
+	}
+
+	private int EmpWageComputation() {
+		int empHrs=0, empWage=0, totalWage = 0, totalEmpHrs=0, totalWorkingDays=0 ;
+		while (totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays){
 			totalWorkingDays++;
 		int empCheck = (int)(Math.random() * 10) %3;
 		switch ((int)empCheck){
@@ -21,15 +30,17 @@ public class EmpWageComputation {
 				empHrs = 0;
 			break;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
+		empWage = empHrs * empRate;
 		totalWage += empWage;
 		System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		System.out.println("Emp Wage: " + empWage);
 		}
-		System.out.println("total Wage is " +totalWage);
-		return totalWage;
+		return totalWage;		
 	}
-	public static void main(String[] args) {
-		computeEmpWage();
-	}
+		public static void main(String[] args){
+		EmpWageComputation dMart = new EmpWageComputation("DMArt", 20, 20, 10);
+		System.out.println("total emp wage for company " +dMart.company + "is" +dMart.EmpWageComputation( ) );
+		EmpWageComputation BigBasket = new EmpWageComputation("BigBasket", 20, 25, 8);
+      System.out.println("total emp wage for company " +BigBasket.company + "is" +BigBasket.EmpWageComputation( ) );
+		}
 }
