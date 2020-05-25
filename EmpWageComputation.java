12e36a3097fdabd3448cb846a95ac71de6512335
vegirevathi <vegirevathi@gmail.com>
2020-05-25 17:39:50 +0530

    Adding new method to compute emp wage in UC7

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,29 +5,31 @@ public class EmpWageComputation {
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_WORKING_HRS = 100;
    
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		int empHrs, empWage, totalWage = 0, totalEmpHrs=0, totalWorkingDays=0 ;
 		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 		int empCheck = (int)(Math.random() * 10) %3;
 		switch ((int)empCheck){
 			case 1:
-				System.out.println("Employee is Full Time Present");
 				empHrs = 8;
 			break;
 			case 2:
-      	   System.out.println("Employee is Part Time Present");
-         	empHrs = 4;
-      	break;
+					empHrs = 4;
+		     	break;
 			default:
-				System.out.println("Employee is Absent");
 				empHrs = 0;
 			break;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		totalWage += empWage;
-		System.out.println("Emp Wage is for that day is: " +empWage);
+		System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
+		System.out.println("Emp Wage: " + empWage);
 		}
 		System.out.println("total Wage is " +totalWage);
+		return totalWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
 }
